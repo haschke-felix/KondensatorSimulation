58bb3a6b72a178beaced2a3d05ee4fc9dc91e277
haschke-felix <haschke.felix@gmail.com>
2021-01-13 21:16:40 +0100

    main setup

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import simulation as sim
 import fieldplot as plt
 
-cap = sim.setupCapacitor(20)
-cap = sim.simulate(cap,10)
+cap = sim.load("cap.npy")
+#cap = sim.setupCapacitor(20)
+#cap = sim.simulate(cap,10)
+print(cap)
 
-plt.printStreamPlot(cap,n=512)
+#sim.save(cap,"cap.npy")
+
+#plt.printDistribution(cap)
+plt.printStreamPlot(cap,n=64)
