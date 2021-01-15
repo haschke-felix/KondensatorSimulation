1506437d76a234647b990c45152e32d9bc75250c
haschke-felix <haschke.felix@gmail.com>
2021-01-15 21:00:55 +0100

    pass arguments to script

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import simulation as sim
 import fieldplot as plt
+import sys
 
 if __name__ == '__main__':
 
-    charges = 50
-    steps = 50
+    charges = 10
+    steps = 10
+    if len(sys.argv) >= 2:
+        charges = sys.argv[0]
+        steps = sys.argv[1]
     cap = sim.setupCapacitor(charges)
-    #print("created")
-    #
-    #th = threadedSim.ThreadedSim(cap)
-    #cap = th.run()
-    #print("threaded ready")
-    
-    
+ 
     cap = sim.simulateThreaded(cap,steps)
     
     file = "cap{}x{}-it{}.npy".format(charges, charges, steps)
