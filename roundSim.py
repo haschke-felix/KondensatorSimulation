d3a4fc9352db54968a45a078fc88e47c1a197c9b
haschke-felix <haschke.felix@gmail.com>
2021-02-01 23:44:17 +0100

    float arg parsing

--- roundSim.py
+++ roundSim.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     steps =1000
     print(sys.argv)
     if len(sys.argv) >= 3:
-        charges = int(sys.argv[1])
+        charges = float(sys.argv[1])
         steps = int(sys.argv[2])
     
     cap = roundCap.setupCapacitor(charges)
@@ -17,7 +17,7 @@ if __name__ == '__main__':
 
     #cap = roundCap.simulateThreaded(cap,steps,step=2)
     for i in range(1,11):
-        cap = roundCap.simulateThreaded(cap,100*i,step=2)
+        cap = roundCap.simulateThreaded(cap,100,step=2)
         file = "roundCap{}x{}-it{}.npy".format(charges, charges, i*100)
         sim.save(cap, path=file)
     if(False):
