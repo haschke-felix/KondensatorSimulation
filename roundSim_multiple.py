d3a9422c71f348cfd92b7f3a977278e499a8aa63
haschke-felix <haschke.felix@gmail.com>
2021-03-07 19:52:47 +0100

    fixup multiple simulation

--- roundSim_multiple.py
+++ roundSim_multiple.py
@@ -8,11 +8,9 @@ if __name__ == '__main__':
     charges = 0.05
     steps =1000
     step = 0.4
-    dist = [0.3,0.4,0.6,0.7,0.9,1.2,1.4,1.6,2]
-    print(sys.argv)
-    
+    distances = [0.3,0.4,0.6,0.7,0.9,1.2,1.4,1.6,2]    
 
-    for d in dist:
+    for dist in distances:
         cap = roundCap.setupCapacitor(charges, dist)
         cap = roundCap.simulateThreaded(cap,steps=steps,step=step, dist= dist,savepath=os.getcwd())
 
