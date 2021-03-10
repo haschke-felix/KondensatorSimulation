33fdf9be2fd748836f84fa544f426b7edc104ee2
haschke-felix <haschke.felix@gmail.com>
2021-03-10 21:09:27 +0100

    direct numpy computation (only on server)

--- feldverteilung.py
+++ feldverteilung.py
@@ -5,17 +5,23 @@ import roundPlateCapacitor as roundCap
 import easygui
 import sys
 
-resolution = 200
+resolution = 60
 if(len(sys.argv) > 1):
     resolution = int(sys.argv[1])    
 
 
 def _fieldProbe(charges, gridMatrix):
-    con = np.subtract(gridMatrix[:,:,:,:,None], charges[:,None,None,None,:],axis=0)
-    E = con / (charges[:,])
-    for charge in charges:
-        con =  gridMatrix - charge[:,None,None,None]
-        E  +=  con / ((charge[0] * np.linalg.norm(con,axis=0)**3)[None,:,:,:])
+    print("gridMatrix", gridMatrix.shape)
+    charges = charges.swapaxes(0,1)
+    print("charges", charges.shape)
+    con = np.subtract(gridMatrix[:,:,:,:,None], charges[:,None,None,None,:])
+    print("con", con.shape)
+    E = con / (charges[0, :] * (np.linalg.norm(con,axis=0)**3)[:,:,:,:])
+    E = np.sum(E,axis=4)
+    print("E", E.shape)
+#    for charge in charges:
+ #       con =  gridMatrix - charge[:,None,None,None]
+  #      E  +=  con / ((charge[0] * np.linalg.norm(con,axis=0)**3)[None,:,:,:])
     return E
 
 def distributionAnalysis(charges,n=512, r=2, dist=1):
@@ -29,10 +35,12 @@ def distributionAnalysis(charges,n=512, r=2, dist=1):
     gridMatrix = np.array(np.meshgrid(p1, p2, p3, indexing = 'ij'))
     print("calculating field")
     E = np.linalg.norm(_fieldProbe(charges, gridMatrix),axis=0)
+    print("E linalg", E.shape)
     
     print("evaluating sum")
     cap_sum = 0
     sum = E.sum()
+    print("sum", sum.shape)
 
     for x_1 in range(0, n):
         print(x_1)
@@ -50,9 +58,9 @@ def distributionAnalysis(charges,n=512, r=2, dist=1):
 
 
 
-
-path = easygui.fileopenbox(default="/home/felix/Documents/Schule/Physik/Facharbeit/Research/Python/Simuliert/rund")
+path = "/home/felix/Documents/Schule/Physik/Facharbeit/Research/Python/Simuliert/rund/0.05/Breite_1.0/cap.npy"
+#path = easygui.fileopenbox(default="/home/felix/Documents/Schule/Physik/Facharbeit/Research/Python/Simuliert/rund")
 #path = "/home/felix/Documents/Schule/Physik/Facharbeit/Research/Python/Simuliert/rund/0.02/Breite_1.0/cap.npy"
 cap = sim.load(path)
 
-distributionAnalysis(cap,n=resolution,r=10, dist=0.4)
+distributionAnalysis(cap,n=resolution,r=10, dist=2)
